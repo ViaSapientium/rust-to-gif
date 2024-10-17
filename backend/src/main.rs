3121f828ebfdfca9b3e9c184841097b8497ffed9
simbld <s_bullado51@hotmail.com>
2024-10-17 04:19:56 +0200

    Create database viasapientium connexion

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpServer, Responder};
+use deadpool_postgres::Pool;
 use dotenvy::dotenv;
 use std::env;
 
@@ -10,13 +11,27 @@ async fn index() -> impl Responder {
   "Rust GIF API is running!"
 }
 
+// Route pour tester la connexion à PostgreSQL
+async fn db_check(pool: web::Data<Pool>) -> impl Responder {
+  match postgres::lib::test_connection(&pool).await {
+    Ok(_) => "PostgreSQL connection successful!",
+    Err(_) => "PostgreSQL connection failed!",
+  }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   dotenv().ok(); // Charge les variables d'environnement à partir de .env
 
+  // Création du pool de connexion PostgreSQL
+  let pool = postgres::config::create_pool();
+
   let port = env::var("PORT").unwrap_or_else(|_| "8081".to_string());
-  HttpServer::new(|| {
-    App::new().route("/", web::get().to(index)) // Route principale
+  HttpServer::new(move || {
+    App::new()
+      .app_data(web::Data::new(pool.clone())) // Ajout du pool dans le contexte
+      .route("/", web::get().to(index)) // Route principale
+      .route("/db-check", web::get().to(db_check)) // Route pour vérifier la connexion à PostgreSQL
   })
   .bind(("127.0.0.1", port.parse().unwrap()))?
   .run()
