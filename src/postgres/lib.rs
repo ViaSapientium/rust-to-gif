c605241f4c37a5ae5264ae6fc99b7f296a8a9d64
simbld <s_bullado51@hotmail.com>
2024-10-17 08:21:16 +0200

    Remove unused code in postgres/lib.rs

--- src/postgres/lib.rs
+++ src/postgres/lib.rs
@@ -1,7 +1,6 @@
 use deadpool_postgres::Pool;
 use std::error::Error;
 
-// Fonction pour tester la connexion au pool PostgresSQL
 pub async fn test_connection(pool: &Pool) -> Result<(), Box<dyn Error>> {
   let client = pool.get().await?;
   client.query("SELECT 1", &[]).await?;
