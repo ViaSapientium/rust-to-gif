1697ca91594c5c364c4085c51b0b7e5d859c8411
simbld <s_bullado51@hotmail.com>
2024-10-21 23:59:36 +0200

    Refactorisation du module de l'utilisateur pour inclure le champ du mot de passe

--- backend/src/postgres/config.rs
+++ backend/src/postgres/config.rs
@@ -1,19 +1,15 @@
-use deadpool_postgres::{Config, ManagerConfig, Pool, RecyclingMethod, Runtime};
-use tokio_postgres::NoTls;
+use deadpool_postgres::{Config, Pool};
 use std::env;
+use tokio_postgres::NoTls;
 
 pub fn create_pool() -> Pool {
   let mut cfg = Config::new();
 
-  cfg.dbname = Some(env::var("PG_DBNAME").unwrap());
-  cfg.user = Some(env::var("PG_USER").unwrap());
-  cfg.password = Some(env::var("PG_PASSWORD").unwrap());
-  cfg.host = Some(env::var("PG_HOST").unwrap());
+  // Chargement des valeurs depuis les variables d'environnement
+  cfg.host = Some(env::var("PG_HOST").expect("PG_HOST must be set"));
+  cfg.user = Some(env::var("PG_USER").expect("PG_USER must be set"));
+  cfg.password = Some(env::var("PG_PASSWORD").expect("PG_PASSWORD must be set"));
+  cfg.dbname = Some(env::var("PG_DBNAME").expect("PG_DBNAME must be set"));
 
-  cfg.manager = Some(ManagerConfig {
-    recycling_method: RecyclingMethod::Fast,
-  });
-
-  // Ajout du runtime pour le nouvel argument
-  cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap()
+  cfg.create_pool(None, NoTls).unwrap()
 }
