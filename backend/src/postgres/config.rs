0717c5e93bee33fab1fd0c5f27a06799b846993c
simbld <s_bullado51@hotmail.com>
2024-10-25 10:32:53 +0200

    Refactor PostgreSQL configuration to load values from environment variables and create a pool

--- backend/src/postgres/config.rs
+++ backend/src/postgres/config.rs
@@ -5,11 +5,12 @@ use tokio_postgres::NoTls;
 pub fn create_pool() -> Pool {
   let mut cfg = Config::new();
 
-  // Chargement des valeurs depuis les variables d'environnement
+  // Loading values from environment variables
   cfg.host = Some(env::var("PG_HOST").expect("PG_HOST must be set"));
   cfg.user = Some(env::var("PG_USER").expect("PG_USER must be set"));
   cfg.password = Some(env::var("PG_PASSWORD").expect("PG_PASSWORD must be set"));
   cfg.dbname = Some(env::var("PG_DBNAME").expect("PG_DBNAME must be set"));
 
+  // Create and return the PostgreSQL pool
   cfg.create_pool(None, NoTls).unwrap()
 }
