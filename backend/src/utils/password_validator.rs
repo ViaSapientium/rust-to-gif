465504ae4bb8cdf22f507dcde617f41117fd7832
simbld <s_bullado51@hotmail.com>
2024-10-31 23:40:03 +0100

    Refactor password_validator.rs to use a lazy static regex for password validation

--- backend/src/utils/password_validator.rs
+++ backend/src/utils/password_validator.rs
@@ -1,6 +1,11 @@
+use once_cell::sync::Lazy;
 use regex::Regex;
 
+const PATTERN: &str = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\p{P})[\p{L}\p{N}\p{P}\p{S}]{12,}$";
+
+static PASSWORD_REGEX: Lazy<Regex> =
+  Lazy::new(|| Regex::new(PATTERN).expect("Échec de la compilation de la regex"));
+
 pub fn validate_password(password: &str) -> bool {
-  let re = Regex::new(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).{12,}$").unwrap();
-  re.is_match(password)
+  PASSWORD_REGEX.is_match(password)
 }
