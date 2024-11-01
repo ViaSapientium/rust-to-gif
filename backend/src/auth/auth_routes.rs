e53e4d09d0c2f92c53e210827a159fd08dbe818d
simbld <s_bullado51@hotmail.com>
2024-11-01 23:50:11 +0100

    Refactor authentication routes to use controller functions for login and registration

--- backend/src/auth/auth_routes.rs
+++ backend/src/auth/auth_routes.rs
@@ -1,9 +1,9 @@
-use crate::auth::auth_service::AuthService;
+use crate::auth::auth_controller::{login, register};
 use actix_web::web;
 
 pub fn configure_auth_routes(cfg: &mut web::ServiceConfig) {
   cfg
-    .route("/validate", web::get().to(AuthService::validate_token))
-    .route("/login", web::post().to(AuthService::login))
-    .route("/register", web::post().to(AuthService::register));
+    .route("/validate", web::get().to(login))
+    .route("/login", web::post().to(login))
+    .route("/register", web::post().to(register));
 }
