15fd4afbf4c6349b9913fda1a33cd995d182a017
simbld <s_bullado51@hotmail.com>
2024-11-02 23:33:26 +0100

    Add password routes configuration to main application

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,6 +5,7 @@ use config::create_pool;
 use deadpool_postgres::Pool;
 use dotenvy::dotenv;
 use user::user_routes::configure_user_routes;
+use utils::password_routes::configure_password_routes;
 // use video::ffmpeg::extract_images;
 // use ffmpeg_next::format::context::input::Input;
 
@@ -13,6 +14,7 @@ mod common;
 mod config;
 mod postgres;
 mod user;
+mod utils;
 // mod video;
 
 // Route simple pour tester le serveur
@@ -52,6 +54,7 @@ async fn main() -> std::io::Result<()> {
       .route("/db_check", web::get().to(db_check))
       .configure(|cfg| configure_auth_routes(cfg))
       .configure(|cfg| configure_user_routes(cfg))
+      .configure(|cfg| configure_password_routes(cfg))
     // .route("/extract_video", web::get().to(extract_video))
   })
   .bind("127.0.0.1:8081")?
